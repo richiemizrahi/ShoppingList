298877da3d8c261ccf51451a49c95bc377cc4b63
Richie Mizrahi <richiemizrahi@gmail.com>
2013-07-22 10:25:01 -0400

    Thinkful project #4

---
+++ application.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+	//Gets whats in the text box and calls the function add
+
+		//click or add an item
+		$("#add").click(add);
+		$("#newItem").keydown(function(enter){
+			if(enter.keyCode == 13){
+				add();
+			}
+		});
+		//check-off an item
+		$("ol").on("click", ".checks", function(){
+			console.log('strike');
+			$(this).parent().find('span').toggleClass('strike');
+			$(this).toggleClass('green red');
+		});
+		//delete an item
+		$("ol").on("click", ".delete", function(){
+			console.log('remove');
+			$(this).closest('li').remove();
+
+		});
+
+	//Sets the info in the text box to variable item.
+	function add(){
+		var item = $('#newItem').val();
+		console.log(item);
+		if(item == '')
+		{
+			alert('You didn\'t enter anything!');
+		}
+		else{
+			var newListItem = '<li class="newItem"><h2><span>'+item+'</span></h2><input type="button" name="delete" class="button delete"><input type="button" name="check" class="button checks green"></li>'
+			$('ol').prepend(newListItem);
+			$('#newItem').val(" ").focus(); 
+		}
+	}
+
+
+	/*function strike(){
+		console.log('strike');
+		$('.newItem span').toggleClass('strike');
+		$('.newItem input').toggleClass('green red');
+
+	}
+
+	function remove(){
+		console.log('remove');
+		var deleteItem = $('this').parent().parent();
+		$('deleteItem').remove();
+	}
+
+	function unstrike(){
+		console.log('unstrike');
+		$('.checkedItem span').toggleClass('strike');
+		$('.checkedItem input').toggleClass('red green');
+	}*/
+
+
+});
